d78b72b2d676da759cae947c800933f0e6cd6382
Cristi Cismas <cristicismas99@gmail.com>
2025-04-13 16:49:00 +0300

    Found another mkd renderer / editor

--- src/components/TabContent/TextContent/index.js
+++ src/components/TabContent/TextContent/index.js
@@ -1,13 +1,20 @@
-import Markdown from "react-markdown";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+
 import styles from "./styles.module.css";
 
 const TextContent = ({ data }) => {
+  const editor = useCreateBlockNote({});
+
+  console.log(editor.document);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{data?.title}</h1>
 
       <div className={styles.markdown}>
-        <Markdown>{data?.text}</Markdown>
+        <BlockNoteView editor={editor} />
       </div>
     </div>
   );
