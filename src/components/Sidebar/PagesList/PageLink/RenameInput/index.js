ddba3f4b9cf183231fbb19f1e5acf9cdd4bdc3f3
Cristi Cismas <cristicismas99@gmail.com>
2025-04-27 10:53:47 +0300

    Extracted RenameInput as a separate component

---
+++ src/components/Sidebar/PagesList/PageLink/RenameInput/index.js
@@ -0,0 +1,79 @@
+import { useEffect, useRef } from "react";
+// components
+import SimpleImage from "@/components/SimpleImage";
+// styles
+import styles from "./styles.module.css";
+
+const RenameInput = ({ onSubmit, onAbort, defaultValue }) => {
+  const inputRef = useRef();
+
+  const handleSubmit = (formData) => {
+    const newTitle = formData.get("rename-input");
+    onSubmit(newTitle);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.select();
+    }
+  }, []);
+
+  useEffect(() => {
+    const closeModalOnEsc = (e) => {
+      if (e.key === "Escape") {
+        onAbort();
+      }
+    };
+
+    document.addEventListener("keydown", closeModalOnEsc);
+
+    return () => {
+      document.removeEventListener("keydown", closeModalOnEsc);
+    };
+  }, []);
+
+  return (
+    <form className={styles.form} action={handleSubmit}>
+      <input
+        defaultValue={defaultValue}
+        ref={inputRef}
+        name="rename-input"
+        title="Empty strings are not allowed in this input"
+        autoComplete="off"
+        pattern="^(?!\s*$).+"
+        required
+        className={styles.input}
+        type="text"
+      />
+
+      <div className={styles.buttons}>
+        <button
+          type="submit"
+          className={`${styles.button} ${styles.confirmButton}`}
+        >
+          <SimpleImage
+            disableLazyLoad
+            src={"/icons/checkmark.svg"}
+            width={24}
+            height={24}
+          />
+        </button>
+
+        <button
+          type="button"
+          onClick={onAbort}
+          className={`${styles.button} ${styles.abortButton}`}
+        >
+          <SimpleImage
+            disableLazyLoad
+            src={"/icons/close.svg"}
+            width={24}
+            height={24}
+          />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default RenameInput;
--- src/components/Sidebar/PagesList/PageLink/index.js
+++ src/components/Sidebar/PagesList/PageLink/index.js
@@ -1,68 +1,12 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 // helpers
 import fetcher from "@/helpers/swrFetcher";
 // components
 import StyledButton from "@/components/StyledButton";
 import SimpleImage from "@/components/SimpleImage";
+import RenameInput from "./RenameInput";
 // styles
 import styles from "./styles.module.css";
-
-const RenameInput = ({ onSubmit, onAbort, defaultValue }) => {
-  const inputRef = useRef();
-
-  const handleSubmit = (formData) => {
-    const newTitle = formData.get("rename-input");
-    onSubmit(newTitle);
-  };
-
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.select();
-    }
-  }, []);
-
-  useEffect(() => {
-    const closeModalOnEsc = (e) => {
-      if (e.key === "Escape") {
-        onAbort();
-      }
-    };
-
-    document.addEventListener("keydown", closeModalOnEsc);
-
-    return () => {
-      document.removeEventListener("keydown", closeModalOnEsc);
-    };
-  }, []);
-
-  return (
-    <form className={styles.form} action={handleSubmit}>
-      <input
-        defaultValue={defaultValue}
-        ref={inputRef}
-        name="rename-input"
-        title="Empty strings are not allowed in this input"
-        autoComplete="off"
-        pattern="^(?!\s*$).+"
-        required
-        className={styles.input}
-        type="text"
-      />
-
-      <button
-        type="submit"
-        className={`${styles.button} ${styles.abortButton}`}
-      >
-        <SimpleImage
-          disableLazyLoad
-          src={"/icons/checkmark.svg"}
-          width={24}
-          height={24}
-        />
-      </button>
-    </form>
-  );
-};
 
 const PageLink = ({ className = "", page, onRename, onDelete }) => {
   const [isRenaming, setIsRenaming] = useState(false);
